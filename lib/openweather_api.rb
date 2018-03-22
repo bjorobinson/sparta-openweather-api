f4ff47123be4bfbc7b594e71ff9561c76d1296a7
Tech-A12 <tech-a12@android-2da98d21d4cff79c.home>
2018-03-22 21:48:18 +0000

    Written first set of tests to pass

--- lib/openweather_api.rb
+++ lib/openweather_api.rb
@@ -1,6 +1,12 @@
+require 'json'
+require 'HTTParty'
 
-class Openweather_api
+class Openweather_API
 
+  attr_reader :openweather_api_london
 
+  def initialize
+    @openweather_api_london = JSON.parse(HTTParty::get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=660fc4ff59cdcee84eb0321163d68677').body)
+  end
 
 end
--- spec/openweather_api_spec.rb
+++ spec/openweather_api_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
-describe Openweather_api do
+describe Openweather_API do
 
   before(:all) do
+    @test_call = Openweather_API.new
+    p @test_call
+  end
+
+  it "should return longitude as an int" do
 
   end
 
-  it "" do
-  
+  it "should return latitude as an int" do
+
+  end
+
+  it "should verify/validate it's London by checking its longitude and latitude vs set coordinates" do
+    
+  end
 end
